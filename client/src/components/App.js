1a67c0fa9901d304450dda9c3f261ea30b94433b
mesbaho <mesbah.omid@gmail.com>
2020-12-11 02:51:39 -0800

    completed registration page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Signup from './Signup'
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
+import { AuthProvider } from '../contexts/AuthContext';
 function App() {
-  return ('Hello World'
-
-  );
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup/>
+        </div>
+      </Container>
+    </AuthProvider>
+  )
 }
 
 export default App;
