f5cfc1a275595a45397430ab6947568d5a61d811
mesbaho <mesbah.omid@gmail.com>
2020-12-11 00:01:16 -0800

    adding registration form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Card, Form, Button} from 'react-bootstrap'
 
-export default function signup() {
+export default function Signup() {
     return (
-        <div>
-
-        </div>
+        <>
+            <Card>
+                <Card.Body>
+                  <h2 className='text-center mb-4'> Register </h2>
+                    <Form>
+                        <Form.Group id='email'>
+                            <Form.Label> Email </Form.Label>
+                            <Form.Control type='email' ref={emailRef} required></Form.Control>
+                        </Form.Group>
+                        <Form.Group id='password'>
+                            <Form.Label> Password </Form.Label>
+                            <Form.Control type='password' ref={passwordRef} required></Form.Control>
+                        </Form.Group>
+                        <Form.Group id='password-confirm'>
+                            <Form.Label> Confirm Password </Form.Label>
+                            <Form.Control type='password' ref={passwordConfirmRef} required></Form.Control>
+                        </Form.Group>
+                        <Button className='w-100' type='submit'>Sign Up</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Existing User? Login
+            </div>
+     </>
     )
 }
