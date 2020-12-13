b710e55a49da14f07d588894c2191f5e199059ae
mesbaho <mesbah.omid@gmail.com>
2020-12-12 23:37:30 -0800

    login and dashboard component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import Signup from './Signup'
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
-import { AuthProvider } from '../contexts/AuthContext';
+import React from "react";
+import Signup from "./Signup";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import {Container} from "react-bootstrap";
+import {AuthProvider} from "../contexts/AuthContext";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup/>
-        </div>
-      </Container>
-    </AuthProvider>
-  )
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{maxWidth: "400px"}}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/register" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
